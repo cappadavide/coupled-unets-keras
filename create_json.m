c083fe6bc807f1a98f925b31fa057f8c900acc67
giusy.r <giusirux@gmail.com>
2022-02-28 14:27:16 +0100

    Aggiornato dataset.json

--- create_json.m
+++ create_json.m
@@ -1,58 +1,55 @@
 %% LOAD DATASETET
 load('mpii_human_pose_v1_u12_1.mat');
 %% MPII DATASET
-newJSON.dataset.MPII.filepath = {};
 nImgs = 1;
 annotations = RELEASE.annolist;
 for i=1:length(annotations)
     if isfield(annotations(i).annorect,'annopoints')==1
-        newJSON.dataset.MPII.filepath  = [newJSON.dataset.MPII.filepath;annotations(i).image.name];
-        for k=1:length(annotations(i).annorect)
-            if isfield(annotations(i).annorect(k).annopoints,'point')==1
-               tempTable = struct2table(annotations(i).annorect(k).annopoints.point);
+        for k=1:length(annotations(i).annorect) % persone
+            if isempty(annotations(i).annorect(k).scale)==0 && isempty(annotations(i).annorect(k).objpos)==0 && isfield(annotations(i).annorect(k).annopoints,'point')==1
+                tempTable = struct2table(annotations(i).annorect(k).annopoints.point);
                 sortedPoints = sortrows(tempTable,'id','ascend');
                 sortedPoints = table2struct(sortedPoints);
-                newJSON.dataset.MPII.people(nImgs).info(k).x1 = annotations(i).annorect(k).x1;
-                newJSON.dataset.MPII.people(nImgs).info(k).y1 = annotations(i).annorect(k).y1;
-                newJSON.dataset.MPII.people(nImgs).info(k).x2 = annotations(i).annorect(k).x2;      
-                newJSON.dataset.MPII.people(nImgs).info(k).y2 = annotations(i).annorect(k).y2;
-                newJSON.dataset.MPII.people(nImgs).info(k).keypoints = repmat(struct('x',0,'y',0,'id',-1),1,16);
-                newJSON.dataset.MPII.people(nImgs).info(k).scale = annotations(i).annorect(k).scale;
+                newJSON.dataset.MPII.people(nImgs).filepath = annotations(i).image.name;
+                newJSON.dataset.MPII.people(nImgs).x1 = annotations(i).annorect(k).x1;
+                newJSON.dataset.MPII.people(nImgs).y1 = annotations(i).annorect(k).y1;
+                newJSON.dataset.MPII.people(nImgs).x2 = annotations(i).annorect(k).x2;      
+                newJSON.dataset.MPII.people(nImgs).y2 = annotations(i).annorect(k).y2;
+                newJSON.dataset.MPII.people(nImgs).keypoints = repmat(struct('x',0,'y',0,'id',-1),1,16);
+                newJSON.dataset.MPII.people(nImgs).scale = annotations(i).annorect(k).scale;
                 objpos.x = annotations(i).annorect(k).objpos.x;
                 objpos.y = annotations(i).annorect(k).objpos.y;
-                newJSON.dataset.MPII.people(nImgs).info(k).objpos = repmat(struct('x',objpos.x,'y',objpos.y),1,1);
+                newJSON.dataset.MPII.people(nImgs).objpos = repmat(struct('x',objpos.x,'y',objpos.y),1,1);
                 count = 1;
                 j=1;
                 while count<=16
                    if j<=length(sortedPoints)
                        if sortedPoints(j).id == count-1
-                           newJSON.dataset.MPII.people(nImgs).info(k).keypoints(count).x = sortedPoints(j).x;
-                           newJSON.dataset.MPII.people(nImgs).info(k).keypoints(count).y = sortedPoints(j).y;
+                           newJSON.dataset.MPII.people(nImgs).keypoints(count).x = sortedPoints(j).x;
+                           newJSON.dataset.MPII.people(nImgs).keypoints(count).y = sortedPoints(j).y;
                            j = j + 1;
                        end
                    end
-                   newJSON.dataset.MPII.people(nImgs).info(k).keypoints(count).id = count-1;
+                   newJSON.dataset.MPII.people(nImgs).keypoints(count).id = count-1;
                    count = count + 1;
                 end
+                nImgs = nImgs + 1;
             end
         end
-        nImgs = nImgs + 1;
     end
 end
 %% LSP DATASET
-newJSON.dataset.LSP.filepath = {};
 load('joints.mat');
 for i=1:2000
-    newJSON.dataset.LSP.filepath = [newJSON.dataset.LSP.filepath;sprintf('img%04d.jpg',i)];
     tmpCellArray = joints(:,:,i);
-    newJSON.dataset.LSP.keypoints(i).points = repmat(struct('x',0,'y',0,'id',-1),1,14);
+    newJSON.dataset.LSP.people(i).filepath = sprintf('img%04d.jpg',i);
     for j=1:14
-        newJSON.dataset.LSP.keypoints(i).points(j).x = tmpCellArray(1,j);
-        newJSON.dataset.LSP.keypoints(i).points(j).y = tmpCellArray(2,j);
-        newJSON.dataset.LSP.keypoints(i).points(j).id = j;
+        newJSON.dataset.LSP.people(i).keypoints(j).x = tmpCellArray(1,j);
+        newJSON.dataset.LSP.people(i).keypoints(j).y = tmpCellArray(2,j);
+        newJSON.dataset.LSP.people(i).keypoints(j).id = j;
     end
 end
 %% CREATE FILE JSON
-%fid = fopen('datasets.json','wt');
-%fprintf(fid,jsonencode(newJSON),'PrettyPrint',true);
-%fclose(fid);
+fid = fopen('datasets.json','wt');
+fprintf(fid,jsonencode(newJSON),'PrettyPrint',true);
+fclose(fid);
--- create_mpii_json.m
+++ create_mpii_json.m
@@ -1,39 +1,38 @@
-newJSON.filepath = {};
 nImgs = 1;
 annotations = RELEASE.annolist;
+%28883
 for i=1:length(annotations)
     if isfield(annotations(i).annorect,'annopoints')==1
-        newJSON.filepath = [newJSON.filepath;annotations(i).image.name];
-        for k=1:length(annotations(i).annorect)
-            if isfield(annotations(i).annorect(k).annopoints,'point')==1
+        for k=1:length(annotations(i).annorect) % persone
+            if isempty(annotations(i).annorect(k).scale)==0 && isempty(annotations(i).annorect(k).objpos)==0 && isfield(annotations(i).annorect(k).annopoints,'point')==1
                 tempTable = struct2table(annotations(i).annorect(k).annopoints.point);
                 sortedPoints = sortrows(tempTable,'id','ascend');
                 sortedPoints = table2struct(sortedPoints);
-                newJSON.people(nImgs).info(k).x1 = annotations(i).annorect(k).x1;
-                newJSON.people(nImgs).info(k).y1 = annotations(i).annorect(k).y1;
-                newJSON.people(nImgs).info(k).x2 = annotations(i).annorect(k).x2;      
-                newJSON.people(nImgs).info(k).y2 = annotations(i).annorect(k).y2;
-                newJSON.people(nImgs).info(k).keypoints = repmat(struct('x',0,'y',0,'id',-1),1,16);
-                newJSON.people(nImgs).info(k).scale = annotations(i).annorect(k).scale;
+                newJSON.people(nImgs).filepath = annotations(i).image.name;
+                newJSON.people(nImgs).x1 = annotations(i).annorect(k).x1;
+                newJSON.people(nImgs).y1 = annotations(i).annorect(k).y1;
+                newJSON.people(nImgs).x2 = annotations(i).annorect(k).x2;      
+                newJSON.people(nImgs).y2 = annotations(i).annorect(k).y2;
+                newJSON.people(nImgs).keypoints = repmat(struct('x',0,'y',0,'id',-1),1,16);
+                newJSON.people(nImgs).scale = annotations(i).annorect(k).scale;
                 objpos.x = annotations(i).annorect(k).objpos.x;
                 objpos.y = annotations(i).annorect(k).objpos.y;
-                newJSON.people(nImgs).info(k).objpos = repmat(struct('x',objpos.x,'y',objpos.y),1,1);
+                newJSON.people(nImgs).objpos = repmat(struct('x',objpos.x,'y',objpos.y),1,1);
                 count = 1;
                 j=1;
                 while count<=16
                    if j<=length(sortedPoints)
                        if sortedPoints(j).id == count-1
-                           newJSON.people(nImgs).info(k).keypoints(count).x = sortedPoints(j).x;
-                           newJSON.people(nImgs).info(k).keypoints(count).y = sortedPoints(j).y;
+                           newJSON.people(nImgs).keypoints(count).x = sortedPoints(j).x;
+                           newJSON.people(nImgs).keypoints(count).y = sortedPoints(j).y;
                            j = j + 1;
                        end
                    end
-                   newJSON.people(nImgs).info(k).keypoints(count).id = count-1;
+                   newJSON.people(nImgs).keypoints(count).id = count-1;
                    count = count + 1;
                 end
+                nImgs = nImgs + 1;
             end
-            
         end
-        nImgs = nImgs + 1;
     end
 end
